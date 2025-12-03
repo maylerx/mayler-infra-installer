bbb528ca6cbaf34e3e12081e17a115c9ffefd7df
maylerx <107667458+maylerx@users.noreply.github.com>
2025-12-03 01:06:31 -0500

    Update 01-base.sh

--- modules/01-base.sh
+++ modules/01-base.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env bash
 module_install() {
-log "Actualizando sistema y instalando herramientas básicas..."
-apt update && apt upgrade -y
-apt install -y build-essential curl wget git vim net-tools
-echo "{\"module\":\"base\",\"packages\":["build-essential","curl","wget","git","vim","net-tools"]}" > "$(dirname "${BASH_SOURCE[0]}")/../manifests/base.json"
-ok "Módulo base instalado."
+    log "Actualizando sistema y instalando herramientas básicas..."
+    apt update && apt upgrade -y
+    apt install -y build-essential curl wget git vim net-tools
+    echo "{\"module\":\"base\",\"packages\":["build-essential","curl","wget","git","vim","net-tools"]}" > "$(dirname "${BASH_SOURCE[0]}")/../manifests/base.json"
+    ok "Módulo base instalado."
 }
 
 
 module_rollback() {
-log "Rollback base: eliminando paquetes instalados (si aplica)..."
-apt remove -y build-essential curl wget git vim net-tools || true
-rm -f "$(dirname "${BASH_SOURCE[0]}")/../manifests/base.json"
+    log "Rollback base: eliminando paquetes instalados (si aplica)..."
+    apt remove -y build-essential curl wget git vim net-tools || true
+    rm -f "$(dirname "${BASH_SOURCE[0]}")/../manifests/base.json"
 }
