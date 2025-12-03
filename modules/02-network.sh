0d292deb46082f1b5d005ed3afd58510e4cafa9c
maylerx <107667458+maylerx@users.noreply.github.com>
2025-12-03 01:06:55 -0500

    Update 02-network.sh

--- modules/02-network.sh
+++ modules/02-network.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 module_install() {
-log "Instalando herramientas de red..."
-apt install -y nmap tcpdump traceroute iperf3 dnsutils whois net-tools
-echo "{\"module\":\"network\"}" > "$(dirname "${BASH_SOURCE[0]}")/../manifests/network.json"
+    log "Instalando herramientas de red..."
+    apt install -y nmap tcpdump traceroute iperf3 dnsutils whois net-tools
+    echo "{\"module\":\"network\"}" > "$(dirname "${BASH_SOURCE[0]}")/../manifests/network.json"
 }
 module_rollback() {
-apt remove -y nmap tcpdump traceroute iperf3 dnsutils whois net-tools || true
-rm -f "$(dirname "${BASH_SOURCE[0]}")/../manifests/network.json"
+    apt remove -y nmap tcpdump traceroute iperf3 dnsutils whois net-tools || true
+    rm -f "$(dirname "${BASH_SOURCE[0]}")/../manifests/network.json"
 }
